62c115f9e01f0424ef5daacf0533f42af71a0075
jaymin212 <57218167+jaymin212@users.noreply.github.com>
2021-12-16 01:40:40 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -45,4 +45,4 @@ def logout():
 
 if __name__ == "__main__":
    port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port, debug=True)
+   app.run(host='182.48.212.91', port=port, debug=True)
